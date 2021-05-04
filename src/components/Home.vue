cbb51fb900b4320d0e6ea2a7187dea7f334c285e
Allan Daile Vida <allandailevida@gmail.com>
2021-05-04 16:31:58 +0800

    [HOME] Added : header navbar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="home">
+    <div>
+      <img src="" alt="">
+
+      <ul class="nav-menu">
+        <li class="nav-a">
+          Home
+        </li>
+   
+      </ul>
+
+      <!-- <ul class="nav-min">
+        <router-link :to="{ name: 'Admin' }" class="nav-a">
+          <font-awesome-icon icon="bars" size="2x" class="hamburger" />
+          </router-link>
+      </ul> -->
+    </div>
+
     <div  class="details details--location">
       <h2 class="details__h2">Hi, I'm Daile</h2>
       <h1 class="details__h1">Web Developer</h1>
@@ -9,11 +26,8 @@
 </template>
 
 <script>
-// @ is an alias to /src
 export default {
   name: "Home",
-  components: {
-  },
 };
 </script>
 
