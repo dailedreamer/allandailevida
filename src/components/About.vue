7b3d581e6c2adcbb899b21a8a5a28f35ab99ffaf
Allan Daile Vida <allandailevida@gmail.com>
2021-05-05 16:14:09 +0800

    [ABOUT] Added : about details and scss file

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+  
+      <h1>About me</h1>
+  
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about",
+};
+</script>
+
+<style lang="scss">
+  @import "../assets/sass/Module/about";
+</style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="portfolio">
     <Home />
+    <About />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Home from "../components/Home";
+import About from "../components/About";
 export default {
   name: "Portfolio",
   components: {
     Home,
+    About,
   },
 };
 </script>
