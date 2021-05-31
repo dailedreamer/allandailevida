135ba90e5ac8967d07399d342756ad42d9e2819e
Allan Daile Vida <allandailevida@gmail.com>
2021-05-31 16:21:00 +0800

    [ABOUT] Added : profile image and profile details content

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="about">
-  
-      <h1>About me</h1>
-  
+    <div class="about-main">
+      <div class="nav__about-me">
+        <label class="lbl__about-me">About me</label>
+      </div>
 
+      <div class="about-me__main">
+        <div class="about-me__img">
+          <img src="../assets/PersonalDetails/profile.jpg" alt="logo">
+        </div>
+        <div class="about-me__details">
+          <h1 class="presonal-details">
+              I'm Allan Daile Vida
+              Agile web developer working with different project
+              In-house Systems in Fujitsu. Daile holds a 
+              Bachelor Degree in Information Technology 
+              from City College of Tagaytay, Philippines.
+          </h1>
+
+          <h1 class="organization-details">
+              I'm proud to have collaborated with 
+              my awesome School and Company:
+          </h1>
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
